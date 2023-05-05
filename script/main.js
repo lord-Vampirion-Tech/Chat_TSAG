63410c19ca4b70c021be612eda38250052d227cc
Dark Lord Technologist <119113939+lord-Vampirion-Tech@users.noreply.github.com>
2023-05-05 03:47:05 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -11,10 +11,43 @@ firebase.initializeApp({
 let userID = "";
 let userRef = "";
 const storageRef = firebase.storage()
+const db = firebase.firestore();
+const usersCollection = db.collection("users");
 
+const input = $('#fileInput');
+const MBLimit = 1024 * 1024; 
 const imgField = $('#img');
 const nameField = $('#name');
-const searchIdField = $('#searchId');
+const passwordField = $('#password');
+const emailField = $('#email');
+const descripdField = $('#descrip');
+
+input.on('change', function () {
+  if (!input.prop('files') && !input.prop('files')[0]) returnz
+  const file = this.files[0];
+  if (file.size > MBLimit) {
+    alert('Размер изображения должен быть не более 1 МБ');
+    input.value = '';
+    return
+  }
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    const img = new Image();
+    img.src = e.target.result;
+
+    img.onload = function () {
+      if (this.width > 0 && this.height > 0) {
+        $('.img').find('img').attr('src', img.src);
+      } else {
+        console.error('Выбранный файл не является изображением')
+      }
+    };
+    img.onerror = function () {
+      console.error('Выбранный файл не является изображением')
+    };
+  };
+  reader.readAsDataURL(file);
+});
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
@@ -24,21 +57,64 @@ firebase.auth().onAuthStateChanged((user) => {
     userRef.get().then(async (doc) => {
       if (doc.exists) {
         const userData = doc.data();
-
         if (userData.color != "") imgField.attr('src', userData.imgURL).css("filter", `sepia(100%) hue-rotate(${userData.color}deg)`)
         else imgField.attr('src', userData.imgURL)
-        nameField.text(userData.name);
-        searchIdField.text(userData.searchId);
-      } else {
-        console.log("ошибка");
+
+
+        if (userData.description != "") descripdField.val(userData.description)
+        nameField.val(userData.name);
+        passwordField.val(userData.password);
+        emailField.val(userData.email);
       }
-    }).catch((error) => {
-      console.error('Ошибка получения данных пользователя:', error);
-    });
+    })
   }
 })
 
-$('#delete').click(async function () {
+$('#save').click(async function () {
+  let imgURL = "";
+  if (await input[0].files.length != 0) {
+    const file = input[0].files[0];
+    const storagePath = 'images/' + userID + '/avatar.png';
+    const fileRef = storageRef.ref().child(storagePath);
+
+    await fileRef.listAll().then(async function (result) {
+      await result.items.forEach(async function (fileRef) {
+        await fileRef.delete()
+        console.log(`фото пользователя с ID ${userID} удалено`);
+        window.location.href = "index.html";
+      });
+    })
+
+    await fileRef.put(file).then(async function (snapshot) {
+      await snapshot.ref.getDownloadURL().then(function (url) {
+        imgURL = url; 
+      }).catch((error) => {
+        console.error('Произошла ошибка: ' + error.message);
+      });
+    }).catch((error) => {
+      console.error('Произошла ошибка: ' + error.message);
+    });
+  }
+  const userDoc = usersCollection.doc(userID);
+
+  if (descripdField.val() == "")return console.log("текст пуст") ;
+
+  userDoc.set({
+    description: descripdField.val(), 
+    name: nameField.val(),
+    password: passwordField.val(),
+    email: emailField.val(), 
+    imgURL: imgURL,
+  }, { merge: true }).then(function () {
+    alert("данные успешно обнавленны и сохранены")
+    console.log("Новые поля успешно добавлены в документ пользователя.");
+  })
+    .catch(function (error) {
+      console.error("Ошибка при добавлении новых полей в документ пользователя: ", error);
+    });
+})
+
+$('#delete-account').click(async function () {
   try {
     await userRef.delete();
     console.log(`Пользователь с ID ${userID} в firestore удален`);
